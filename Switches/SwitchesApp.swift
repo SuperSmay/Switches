e485a3ae0c5ffd12c2f29a1390e9727d28bab7f6
Sam Askling <samaskling@gmail.com>
2022-07-02 01:06:19 -0500

    Initial Commit

---
+++ Switches/SwitchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchesApp.swift
+//  Switches
+//
+//  Created by Sam Askling on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwitchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
